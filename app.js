ca435d3dfe9b1e56c7bdc9fa0fcf9ddad04c182e
James Otwane <127046977+ja259@users.noreply.github.com>
2023-06-24 14:24:11 -0400

    Add index.html

---
+++ app.js
@@ -0,0 +1,29 @@
+
+const gamesBoardContainer = document.querySelector('#gamesboard-container')
+const optionContainer = document.querySelector('.option-container')
+const flipButton = document.querySelector('#flip-button')
+
+// Option choosing
+let angle = 0
+function flip() {
+    const optionShips = Array.from(optionContainer.children)
+    if (angle === 0) {
+        angle = 90
+    } else {
+        angle = 0
+    }
+    optionShips.forEach(optionShip => optionShip.style.transform = `rotate(${angle}deg)`)
+}
+
+// Creating Boards
+const width = 10
+
+function createBoard() {
+    const gamesBoardContainer = document.createElement('div')
+    gamesBoardContainer.classList.add('game-board')
+    gamesBoardContainer.style.backgroundColor = 'pink'
+
+    gamesBoardContainer.append(gamesBoardContainer)
+}
+
+flipButton.addEventListener('click', flip)
